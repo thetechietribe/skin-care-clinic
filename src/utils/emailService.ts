1edab5fe0ffde95cee2f262969c68af839118643
hassantechiee <ihassanchaudhary11@gmail.com>
2024-10-21 06:33:11 -0700

    initialize branch

---
+++ src/utils/emailService.ts
@@ -0,0 +1,27 @@
+import emailjs from "@emailjs/browser";
+
+interface formDataInrFace {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  description: string;
+  refFirstName: string;
+  refLastName: string;
+  [key: string]: any;
+}
+
+const sendEmail = async (serviceId : string, templateId : string, publicKey : string,  templateParams:formDataInrFace) => {
+  try {
+    emailjs.init(publicKey);
+    const response = await emailjs.send(serviceId, templateId, templateParams);
+    console.log("Email sent successfully:", response);
+    return true;
+  } catch (error) {
+    console.error("Failed to send email:", error);
+    throw new Error("Email sending failed");
+  }
+};
+
+
+export default sendEmail;
